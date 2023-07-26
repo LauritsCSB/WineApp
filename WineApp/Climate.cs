477091e5dbe6bac7ff834b3cf6d4253ab929e7b7
Laurits Bentsen <laurits2@hotmail.com>
2023-07-26 21:09:50 +0200

    prevailingclimate renamed to climate

---
+++ WineApp/Climate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WineApp
+{
+    public class PrevailingClimate
+    {
+        public List<int> temperatureMin { get; set; }
+        public List<int> temperatureMax { get; set; }
+        public List<double> temperatureAverage { get; set; }
+        public int averageGDD { get; set; }
+        public int sunshineHoursAverage { get; set; }
+        public int rainfallAverage { get; set; }
+        public List<int> diurnalTemperatureVariation { get; set; }
+        public List<int> humidutyLevelAverage { get; set; }
+        public List<string> frostRiskAverage { get; set; }
+    }
+}
+
