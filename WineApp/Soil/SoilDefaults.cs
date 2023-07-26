eb259ea111a45fcec540696b3b0cd1b84944d0c6
Laurits Bentsen <laurits2@hotmail.com>
2023-07-26 20:59:14 +0200

    soildefaults changed to single "list"
    and changed again into an enum

--- WineApp/Soil/SoilDefaults.cs
+++ WineApp/Soil/SoilDefaults.cs
@@ -1,85 +1,49 @@
 ﻿namespace WineApp.SoilComposition
 {
-    public class SoilDefaults
+    public static class SoilDefaults
     {
-        public static List<string> SoilTypesA_C
+        public enum SoilTypes
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Albariza",
-                    "Alluvium",
-                    "Basalt",
-                    "Boulbènes",
-                    "Brickearth",
-                    "Calcareous soil",
-                    "Carbonaceous soil",
-                    "Chalk",
-                    "Clay"
-                };
-            }
-        }
-
-        public static List<string> SoilTypesD_H
-        {
-            get
-            {
-                return new List<string>()
-                {
-                    "Dolomite",
-                    "Flint",
-                    "Galestro",
-                    "Granite",
-                    "Gravel",
-                    "Greywacke",
-                    "Gypsum",
-                    "Hardpan"
-                };
-            }
-        }
-
-        public static List<string> SoilTypesI_Q
-        {
-            get
-            {
-                return new List<string>()
-                {
-                    "Keuper",
-                    "Kimmeridgian soil",
-                    "Lignite",
-                    "Limestone",
-                    "Llicorella",
-                    "Loam",
-                    "Loess",
-                    "Marl",
-                    "Mica",
-                    "Muschelkalk",
-                    "Perlite",
-                    "Quartz"
-                };
-            }
-        }
-
-        public static List<string> SoilTypesS_Z
-        {
-            get
-            {
-                return new List<string>()
-                {
-                    "Sand",
-                    "Schist",
-                    "Shale",
-                    "Siliceous soil",
-                    "Silt",
-                    "Silex",
-                    "Slate",
-                    "Terra Rossa",
-                    "Tufa",
-                    "Ultisol",
-                    "Volcanic soil"
-                };
-            }
+            Albariza,
+            Alluvium,
+            Basalt,
+            Boulbènes,
+            Brickearth,
+            Calcareous,
+            Carbonaceous,
+            Chalk,
+            Clay,
+            Dolomite,
+            Flint,
+            Galestro,
+            Granite,
+            Gravel,
+            Greywacke,
+            Gypsum,
+            Hardpan,
+            Keuper,
+            Kimmeridgian,
+            Lignite,
+            Limestone,
+            Llicorella,
+            Loam,
+            Loess,
+            Marl,
+            Mica,
+            Muschelkalk,
+            Perlite,
+            Quartz,
+            Sand,
+            Schist,
+            Shale,
+            Siliceous,
+            Silt,
+            Silex,
+            Slate,
+            TerraRossa,
+            Tufa,
+            Ultisol,
+            Volcanic
         }
     }
 }
