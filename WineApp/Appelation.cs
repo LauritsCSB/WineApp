fbb6e3adf2782f8c6cfbc2f9cd5163ab9374bf7c
Laurits Bentsen <laurits2@hotmail.com>
2023-07-27 08:22:14 +0200

    trimmed appelation class
    it now only has a region property giving it access upwards.
    removed properties for crop yield since it's only relevant for vineyards, it can be added later with a calculation if important

--- WineApp/Appelation.cs
+++ WineApp/Appelation.cs
@@ -2,17 +2,8 @@
 {
     public class Appelation
     {
-        public string nameOfAppelation { get; set; }
-        public Country countryOfLocation { get; set; }
+        public string nameOfAppelation { get; set; }     
         public Region regionOfLocation { get; set; }
-        public PrevailingClimate GeneralClimateOfArea { get; set; }
-        public List<MicroClimate> MicroClimatesOfArea { get; set; }
-        public List<SoilComposition.SoilDefaults> soilsOfAppelation { get; set; }
-        public List<Terroir> terroirsOfAppelation { get; set; }
-        public List<Grape> GrapeVarietiesOfAppelation { get; set; }
-        public List<Grape> wineStylesOfAppelation { get; set; }
-        public string cropSizeAverageOfAppelation { get; set; }
-        public string cropYieldAverageOfAppelation { get; set; }
         public string historicalSignificanceOfAppelation { get; set; }
         public string reputationAndQualityOfAppelation { get; set; }
     }
