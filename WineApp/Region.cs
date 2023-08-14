a5ce8e9b71e0337f27de1fec987c9bd4aa5e4f72
Laurits Bentsen <laurits2@hotmail.com>
2023-08-14 19:31:46 +0200

    removed a lot of unnecessary fields
    like changes in other classes, these fields could be cool to add again later

--- WineApp/Region.cs
+++ WineApp/Region.cs
@@ -1,20 +1,10 @@
-﻿using System;
-namespace WineApp
+﻿namespace WineApp
 {
     public class Region
     {
-        public string nameOfRegion { get; set; }
-        public Country countryOfRegion { get; set; }
-        public List<Grape> grapeVarietiesInRegion { get; set; }
-        public PrevailingClimate generalClimateOfRegion { get; set; }
-        public List<MicroClimate> microClimatesInRegion { get; set; }
-        public List<SoilComposition.SoilDefaults> generalSoilTypesInRegion { get; set; }
-        public string generalTopographyOfRegion { get; set; }
-        public string historicalSignificanceOfRegion { get; set; }
-        public List<Grape> commonWineStylesInRegion { get; set; }
-        public List<Vineyard> notableProducersOfRegion { get; set; }
-        public string regionalWindPatternInRegion { get; set; }
-        public string wineMakingRegulationOfRegion { get; set; }
+        public string Name { get; set; }
+        public Country Country { get; set; }
+        public Climate Climate { get; set; }
     }
 }
 
