afc1970bfc9ca06ff8f63ee2c7f09b76d141a0fa
Laurits Bentsen <laurits2@hotmail.com>
2023-08-14 19:30:06 +0200

    changed naming and removed fields
    that aren't needed in first version of app
    most of removed fields could be nice to implement in later versions

--- WineApp/Grape.cs
+++ WineApp/Grape.cs
@@ -2,20 +2,24 @@
 {
     public class Grape
     {
-        public List<string> nameOfGrape { get; set; }
-        public string placeOfOrigin { get; set; }
-        public List<Flavor.FlavorDefaults> primaryFlavors { get; set; }
-        public List<Aroma.AromaDefaults> primaryAromas { get; set; }
-        public Flavor.FlavorDefaults acidityInGeneral { get; set; }
-        public Body.BodyDefaults tanninsInGeneral { get; set; }
-        public List<Region> growingRegions { get; set; }
-        public PrevailingClimate climateSuitability { get; set; }
-        public Terroir optimalGrowingConditions { get; set; }
-        public List<string> typicalWineStyles { get; set; }
-        public List<string> wineAgingPotential { get; set; }
-        public List<string> foodPairing { get; set; }
-        public Vineyard notableProducers { get; set; }
-        public string historicalSignificance { get; set; }
-        public string currentTrends { get; set; }
+        public string Name { get; set; }
+        public Appearance Appearance { get; set; }
+        public Aroma Aroma { get; set; }
+        public Flavor Flavor { get; set; }
+        public Body Body { get; set; }
+
+        public enum WineTypes //placed here or defaults?
+        {
+            Red,
+            White,
+            Rosé,
+            Orange,
+            Sparkling,
+            Fortified,
+            Ice,
+            Dessert
+        }
+
+        public int AgingPotential { get; set; }
     }
 }
