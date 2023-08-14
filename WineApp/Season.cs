5941c767798f5e4ffdee41af533b2755c5a7449b
Laurits Bentsen <laurits2@hotmail.com>
2023-08-14 19:32:38 +0200

    removed unnecessary fields
    only fields with value to core function of first version has been left

--- WineApp/Season.cs
+++ WineApp/Season.cs
@@ -3,21 +3,16 @@
     public class Season
     {
         public DateOnly YearOfSeason { get; set; }
-        public Region regionName { get; set; }
-        public List<int> monthlyRainfallOfSeason { get; set; }
-        public List<int> monthlyTemperatureAverageInSeason { get; set; }
-        public List<Climate> sunlightExposureInSeason { get; set; }
-        public List<Climate> growingDegreeDaysInSeason { get; set; }
-        public int daysOfFrostInSeason { get; set; }
-        public int daysOfHailInSeason { get; set; }
-        public string grapeDevelopmentEvaluation { get; set; }
-        public string sizeOfCropInSeason { get; set; }
-        public string yieldOfCropInSeason { get; set; }
-        public string grapeHealthInSeason { get; set; }
-        public string budbreakTimingOfSeason { get; set; }
-        public string harvestTimingOfSeason { get; set; }
-        public string overallVintageQuality { get; set; }
-        public string agingPotentialOfWinesInSeason { get; set; }
+        public Region RegionName { get; set; }
+
+        public List<double> MonthlyTemperatureAverage { get; set; }
+        public List<double> MonthlyTemperatureHigh { get; set; }
+        public List<double> MonthlyTemperatureLow { get; set; }
+        public List<double> MonthlyRainfall { get; set; }
+
+        public bool FrostDuringBudbreak { get; set; }
+        public int SpringDaysOfFrost { get; set; }
+        public int AutumnDaysOfFrost { get; set; }
     }
 }
 
