b451859869d4a621da3de96e83d975b95f44f8a6
Laurits Bentsen <laurits2@hotmail.com>
2023-08-14 19:26:05 +0200

    removed several fields
    scores can be added in later versions
    the same with notes and awards

--- WineApp/Bottle.cs
+++ WineApp/Bottle.cs
@@ -6,14 +6,7 @@
         public List<Grape> Grapes { get; set; }
         public Vineyard Vineyard { get; set; }
         public DateOnly Vintage { get; set; }
-        public int ProfessionalScore { get; set; }
-        public int AmateurScore { get; set; }
         public int AlcoholPercentage { get; set; }
-        public string DrinkOrHold { get; set; }
-        public List<string> ProfessionalNotes { get; set; }
-        public List<string> AmateurNotes { get; set; }
-        public string WinemakersNote { get; set; }
         public Vinification Vinification { get; set; }
-        public List<string> Awards { get; set; }
     }
 }
