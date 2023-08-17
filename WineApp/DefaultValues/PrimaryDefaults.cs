55bb4872254acd497d1701ee920b0ea332946ad1
Laurits Bentsen <laurits2@hotmail.com>
2023-08-17 13:19:43 +0200

    changed data type to enum

--- WineApp/DefaultValues/PrimaryDefaults.cs
+++ WineApp/DefaultValues/PrimaryDefaults.cs
@@ -2,182 +2,123 @@
 {
     public static class PrimaryDefaults
     {
-        public static List<string> Flower
+        public enum Flower
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Iris",
-                    "Peony",
-                    "Elderflower",
-                    "Arcacia",
-                    "Lilac",
-                    "Jasmine",
-                    "Honeysuckle",
-                    "Violet",
-                    "Lavender",
-                    "Rose",
-                    "Potpourri",
-                    "Hibiscus",
-                    "Citrus",
-                    "Lime",
-                    "Lemon",
-                    "Grapefruit",
-                    "Orange",
-                    "Marmalade"
-                };
-            }
+            Iris,
+            Peony,
+            Elderflower,
+            Arcacia,
+            Lilac,
+            Jasmine,
+            Honeysuckle,
+            Violet,
+            Lavender,
+            Rose,
+            Potpourri,
+            Hibiscus,
+            Citrus,
+            Lime,
+            Lemon,
+            Grapefruit,
+            Orange,
+            Marmalade
         }
 
-        public static List<string> TreeFruit
+        public enum TreeFruit
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Quince",
-                    "Apple",
-                    "Pear",
-                    "Nectarine",
-                    "Peach",
-                    "Apricot",
-                    "Persimmon"
-                };
-            }
+            Quince,
+            Apple,
+            Pear,
+            Nectarine,
+            Peach,
+            Apricot,
+            Persimmon
         }
 
-        public static List<string> TropicalFruit
+        public enum TropicalFruit
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Pineapple",
-                    "Mango",
-                    "Guava",
-                    "Passion Fruit",
-                    "Lychee",
-                    "Bubblegum"
-                };
-            }
+            Pineapple,
+            Mango,
+            Guava,
+            PassionFruit,
+            Lychee,
+            Bubblegum
         }
 
-        public static List<string> RedFruit
+        public enum RedFruit
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Cranberry",
-                    "Red Plum",
-                    "Pomegranate",
-                    "Sour Cherry",
-                    "Strawberry",
-                    "Cherry",
-                    "Raspberry"
-                };
-            }
+            Cranberry,
+            RedPlum,
+            Pomegranate,
+            SourCherry,
+            Strawberry,
+            Cherry,
+            Raspberry
         }
 
-        public static List<string> BlackFruit
+        public enum BlackFruit
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Boysenberry",
-                    "Black Currant",
-                    "Black Cherry",
-                    "Plum",
-                    "Blackberry",
-                    "Blueberry",
-                    "Olive"
-                };
-            }
+            Boysenberry,
+            BlackCurrant,
+            BlackCherry,
+            Plum,
+            Blackberry,
+            Blueberry,
+            Olive
+                
         }
 
-        public static List<string> DriedFruit
+        public enum DriedFruit
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Raisin",
-                    "Fig",
-                    "Date",
-                    "Fruitcake"
-                };
-            }
+            Raisin,
+            Fig,
+            Date,
+            Fruitcake
         }
 
-        public static List<string> NobleRot
+        public enum NobleRot
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Beeswax",
-                    "Ginger",
-                    "Honey"
-                };
-            }
+            Beeswax,
+            Ginger,
+            Honey
         }
 
-        public static List<string> Spice
+        public enum Spice
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "White Pepper",
-                    "Red Pepper",
-                    "Black Pepper",
-                    "Cinnamon",
-                    "Anise",
-                    "5-Spice",
-                    "Fennel",
-                    "Eucalyptus",
-                    "Mint",
-                    "Thyme"
-                };
-            }
+            WhitePepper,
+            RedPepper,
+            BlackPepper,
+            Cinnamon,
+            Anise,
+            FiveSpice,
+            Fennel,
+            Eucalyptus,
+            Mint,
+            Thyme
         }
 
-        public static List<string> Vegetable
+        public enum Vegetable
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Grass",
-                    "Tomato Leaf",
-                    "Gooseberry",
-                    "Bell Pepper",
-                    "Jalapeño",
-                    "Bitter Almond",
-                    "Tomato",
-                    "Sun-Dried Tomato",
-                    "Black Tea"
-                };
-            }
+            Grass,
+            TomatoLeaf,
+            Gooseberry,
+            BellPepper,
+            Jalapeño,
+            BitterAlmond,
+            Tomato,
+            SunDriedTomato,
+            BlackTea
         }
 
-        public static List<string> Earth
+        public enum Earth
         {
-            get
-            {
-                return new List<string>()
-                {
-                    "Clay Pot",
-                    "Slate",
-                    "Wet Gravel",
-                    "Potting Soil",
-                    "Red Beet",
-                    "Volcanic Rocks",
-                    "Petroleum"
-                };
-            }
+            ClayPot,
+            Slate,
+            WetGravel,
+            PottingSoil,
+            RedBeet,
+            VolcanicRocks,
+            Petroleum
         }
     }
 }
