93e0fb094fd8979b7697fba7603c015cc045b2f3
Laurits Bentsen <laurits2@hotmail.com>
2023-08-17 13:19:24 +0200

    changed data type to enum

--- WineApp/DefaultValues/FlavorDefaults.cs
+++ WineApp/DefaultValues/FlavorDefaults.cs
@@ -2,50 +2,33 @@
 {
     public static class FlavorDefaults
     {
-        public static List<string> BalanceVariation
+        public enum Balance
         {
-            get
-            {
-                return new List<string>()
-                    {
-                        "Well-Balanced",
-                        "Balanced",
-                        "Slightly Off-Balance",
-                        "Off Balance"
-                    };
-            }
+                        WellBalanced,
+                        Balanced,
+                        SlightlyOffBalance,
+                        OffBalance
+                    
         }
 
-        public static List<string> SweetnessVariation
+        public enum Sweetness
         {
-            get
-            {
-                return new List<string>()
-                    {
-                        "Dry",
-                        "Off-Dry",
-                        "Medium-Dry",
-                        "Medium-Sweet",
-                        "Sweet",
-                        "Dessert"
-                    };
-            }
+                        Dry,
+                        OffDry,
+                        MediumDry,
+                        MediumSweet,
+                        Sweet,
+                        Dessert
         }
 
-        public static List<string> FinishVariation
+        public enum Finish
         {
-            get
-            {
-                return new List<string>()
-                    {
-                        "Short Finish",
-                        "Medium Finish",
-                        "Long Finish",
-                        "Lingering Finish",
-                        "Complex Finish",
-                        "Fading Finish"
-                    };
-            }
+                        Short,
+                        Medium,
+                        Long,
+                        Lingering,
+                        Complex,
+                        Fading
         }
     }
 }
