85de900ec143d4e3db11048b4f33431d7f232d74
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:08:28 +0200

    added winetype default values class

---
+++ WineApp/DefaultValues/WineTypeDefaults.cs
@@ -0,0 +1,18 @@
+﻿namespace WineApp.DefaultValues
+{
+    public static class WineTypeDefaults
+    {
+        public enum WineTypes
+        {
+            Red,
+            White,
+            Rosé,
+            Orange,
+            Sparkling,
+            Fortified,
+            Ice,
+            Dessert
+        }
+    }
+}
+
