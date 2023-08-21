19bca5e80a7226a3678665d0b424d2985530c4b1
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:00:02 +0200

    Added address class

---
+++ WineApp/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace WineApp
+{
+    public class Address
+    {
+        public string HouseNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string State { get; set; }
+        public Country Country { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
+
