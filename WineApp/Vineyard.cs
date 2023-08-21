e4a6f3541acfc690583fde18e22bbcb2eb293dc2
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:15:07 +0200

    Added address field
    added soiltype fields with reference to default soiltypes class
    removed climate field, region has climate

--- WineApp/Vineyard.cs
+++ WineApp/Vineyard.cs
@@ -3,14 +3,12 @@
     public class Vineyard
     {
         public string Name { get; set; }
-        public string Address { get; set; }
+        public Address Address { get; set; } 
         public Appelation Appelation { get; set; }
-        public Region Region { get; set; }
-        public List<string> SoilTypes { get; set; }
+        public DefaultValues.SoilDefaults.SoilTypes SoilTypes { get; set; }
         public int Altitude { get; set; }
         public int Latitude { get; set; }
         public int ProximityToBodyOfWater { get; set; }
-        public Climate Climate { get; set; }
         public string Topography { get; set; }
         public double SlopeAngle { get; set; }
         public string CardinalDirectionOfSlope { get; set; }
