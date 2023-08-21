a5a3473730e9e9a65c2747dc1372408193146066
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:13:38 +0200

    Added tertiary values class
    all fields relates to relevant defaults class

---
+++ WineApp/TertiaryValues.cs
@@ -0,0 +1,9 @@
+﻿namespace WineApp
+{
+    public class TertiaryValues
+    {
+        public DefaultValues.TertiaryDefaults.Oak GetOak { get; set; }
+        public DefaultValues.TertiaryDefaults.GeneralAging GetGeneralAging { get; set; }
+    }
+}
+
