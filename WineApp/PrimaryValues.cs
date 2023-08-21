3ead9e5784967706b5fb9fa422f0e2952d9d40ba
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:13:10 +0200

    Added primary values class
    All fields relates to relevant defaults class

---
+++ WineApp/PrimaryValues.cs
@@ -0,0 +1,17 @@
+﻿namespace WineApp
+{
+    public class PrimaryValues
+    {
+        public DefaultValues.PrimaryDefaults.Flower GetFlower { get; set; }
+        public DefaultValues.PrimaryDefaults.TreeFruit GetTreeFruit { get; set; }
+        public DefaultValues.PrimaryDefaults.TropicalFruit GetTropicalFruit { get; set; }
+        public DefaultValues.PrimaryDefaults.RedFruit GetRedFruit { get; set; }
+        public DefaultValues.PrimaryDefaults.BlackFruit GetBlackFruit { get; set; }
+        public DefaultValues.PrimaryDefaults.DriedFruit GetDriedFruit { get; set; }
+        public DefaultValues.PrimaryDefaults.NobleRot GetNobleRot { get; set; }
+        public DefaultValues.PrimaryDefaults.Spice GetSpice { get; set; }
+        public DefaultValues.PrimaryDefaults.Vegetable GetVegetable { get; set; }
+        public DefaultValues.PrimaryDefaults.Earth GetEarth { get; set; }
+    }
+}
+
