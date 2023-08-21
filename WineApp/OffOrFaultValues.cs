70bc53098273773cb0da8dc45ca5ed21cd827cde
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:10:29 +0200

    added OffOrFaultValues class
    fields references default values class FaultDefaults

---
+++ WineApp/OffOrFaultValues.cs
@@ -0,0 +1,12 @@
+﻿namespace WineApp
+{
+    public class OffOrFaultValues
+    {
+        public DefaultValues.FaultDefaults.CorkTaint GetCork { get; set; }
+        public DefaultValues.FaultDefaults.Cooked GetCooked { get; set; }
+        public DefaultValues.FaultDefaults.VolatilyAcidity GetVolatilyAcidity { get; set; }
+        public DefaultValues.FaultDefaults.SulfidesAndMerceptans GetSulfidesAndMerceptans { get; set; }
+        public DefaultValues.FaultDefaults.Brettanomyce GetBrettanomyce { get; set; }
+    }
+}
+
