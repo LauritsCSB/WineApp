78ece13790db8e3d792909cb0f7c8c5fb2ebeb7b
Laurits Bentsen <laurits2@hotmail.com>
2023-08-21 16:09:08 +0200

    removed winetypes enum and added winetypes field referencing winetypes default class

--- WineApp/Grape.cs
+++ WineApp/Grape.cs
@@ -3,23 +3,11 @@
     public class Grape
     {
         public string Name { get; set; }
-        public Appearance Appearance { get; set; }
+        public Appearance Appearance { get; set; } //TODO Check API's and data for this info
         public Aroma Aroma { get; set; }
         public Flavor Flavor { get; set; }
         public Body Body { get; set; }
-
-        public enum WineTypes //placed here or defaults?
-        {
-            Red,
-            White,
-            Rosé,
-            Orange,
-            Sparkling,
-            Fortified,
-            Ice,
-            Dessert
-        }
-
+        public DefaultValues.WineTypeDefaults.WineTypes WineTypes { get; set; }
         public int AgingPotential { get; set; }
     }
 }
