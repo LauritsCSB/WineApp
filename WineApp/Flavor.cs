d4de01725509b2a60c2ea72d66434b4c50b62d81
Laurits Bentsen <laurits2@hotmail.com>
2023-11-09 12:08:14 +0100

    removed sweetness field
    moved to structure class

--- WineApp/Flavor.cs
+++ WineApp/Flavor.cs
@@ -7,7 +7,6 @@
         public TertiaryValues TertiaryFlavors { get; set; }
         public OffOrFaultValues OffOrFaultFlavors { get; set; }
         public DefaultValues.FlavorDefaults.Balance FlavorBalance { get; set; }
-        public DefaultValues.FlavorDefaults.Sweetness SweetnessLevel { get; set; }
         public DefaultValues.FlavorDefaults.Finish FinishLength { get; set; }
         public DefaultValues.FlavorDefaults.Complexity FlavorComplexity { get; set; }
     }
