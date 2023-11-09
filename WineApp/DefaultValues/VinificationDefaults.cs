aae0fe8d217ec75b81d60ef1aed81d1bfe655fd6
Laurits Bentsen <laurits2@hotmail.com>
2023-11-09 12:30:01 +0100

    Added default values in vinification defaults class

---
+++ WineApp/DefaultValues/VinificationDefaults.cs
@@ -0,0 +1,21 @@
+﻿namespace WineApp.DefaultValues
+{
+    public static class VinificationDefaults
+    {
+        public enum HarvestMethod
+        {
+            Mechanical,
+            Hand
+        }
+
+        public enum SortingMethod
+        {
+            Manual,
+            Mechanical,
+            Gravity,
+            Vibration,
+            Density
+        }
+    }
+}
+
