a1f7699151a385beda2dc8841455fb75f536d946
Laurits Bentsen <laurits2@hotmail.com>
2023-11-09 12:28:24 +0100

    added structure and structure defaults classes
    most fields has been taken from other classes

---
+++ WineApp/DefaultValues/StructureDefaults.cs
@@ -0,0 +1,48 @@
+﻿namespace WineApp.DefaultValues
+{
+    public static class StructureDefaults
+    {
+        public enum Sweetness
+        {
+            BoneDry,
+            Dry,
+            OffDry,
+            Sweet,
+            VerySweet,
+        }
+
+        public enum Tannin
+        {
+            Low,
+            Moderate,
+            High,
+            VeryHigh
+        }
+
+        public enum Acid
+        {
+            Low,
+            Medium,
+            High
+        }
+
+        public enum AlcoholFeel
+        {
+            Light,
+            Moderate,
+            Warm,
+            Hot
+        }
+
+        public enum Body
+        {
+            Light,
+            Medium,
+            Full,
+            Creamy,
+            Round,
+            Lean
+        }
+    }
+}
+
---
+++ WineApp/Structure.cs
@@ -0,0 +1,12 @@
+﻿namespace WineApp
+{
+    public class Body
+    {
+        public DefaultValues.StructureDefaults.Sweetness Sweetness { get; set; }
+        public DefaultValues.StructureDefaults.Tannin Tannin { get; set; }
+        public DefaultValues.StructureDefaults.Acid Acid { get; set; }
+        public DefaultValues.StructureDefaults.AlcoholFeel AlcoholFeel { get; set; }
+        public DefaultValues.StructureDefaults.Body Texture { get; set; }
+    }
+}
+
