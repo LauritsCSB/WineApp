ff1b95d557acdcc6ec90cdaab50c5d87c2b9b0dd
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:06:55 +0100

    Changed AromaDefaults to NoseDefaults
    Edited members to mimick those on WSET level 4 tasting chart

---
+++ WineApp/DefaultValues/NoseDefaults.cs
@@ -0,0 +1,21 @@
+﻿namespace WineApp.DefaultValues
+{
+    public static class NoseDefaults
+    {
+        public enum Condition
+        {
+            Clean,
+            Unclean
+        }
+
+        public enum Intensity
+        {
+            Light,
+            MediumMinus,
+            Medium,
+            MediumPlus,
+            Pronounced
+        }
+    }
+}
+
