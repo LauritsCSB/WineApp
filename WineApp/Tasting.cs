0627b0c4f21d7819932768bf791bdacae3c159eb
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:12:03 +0100

    Updated naming of some members, removed place

--- WineApp/Tasting.cs
+++ WineApp/Tasting.cs
@@ -3,13 +3,11 @@
     public class Tasting
     {
         public DateTime Date { get; set; }
-        public string Place { get; set; }
         public string Occasion { get; set; }
         public Bottle Bottle { get; set; }
         public Appearance Appearance { get; set; }
-        public Aroma Aroma { get; set; }
-        public Flavor Flavor { get; set; }
-        public Body Body { get; set; }
+        public Nose Nose { get; set; }
+        public Palate Palate { get; set; }
     }
 }
 
