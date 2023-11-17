f64d58685040be4c36853d6818280c0d939a49d1
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:04:19 +0100

    Renamed Aroma class to Nose
    Edited members to mimick those on WSET level 4 tasting chart

---
+++ WineApp/Nose.cs
@@ -0,0 +1,13 @@
+﻿namespace WineApp
+{
+    public class Nose
+    {
+        public DefaultValues.NoseDefaults.Condition Condition { get; set; }
+        public DefaultValues.NoseDefaults.Intensity Intensity { get; set; }
+        public PrimaryValues PrimaryAromas { get; set; }
+        public DefaultValues.SecondaryDefaults.Microbial SecondaryAromas { get; set; }
+        public TertiaryValues TertiaryAromas { get; set; }
+        public FaultValues FaultAromas { get; set; }
+    }
+}
+
