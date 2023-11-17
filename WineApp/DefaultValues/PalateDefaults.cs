dc4e9145baa13da200899d78af543558bd420594
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:07:14 +0100

    Changed FlavorDefaults to PalateDEfaults
    Edited members to mimick those on WSET level 4 tasting chart

---
+++ WineApp/DefaultValues/PalateDefaults.cs
@@ -0,0 +1,46 @@
+﻿namespace WineApp.DefaultValues
+{
+    public static class PalateDefaults
+    {
+        public enum Sweetness
+        {
+            Dry,
+            OffDry,
+            MediumDry,
+            MediumSweet,
+            Sweet,
+            Luscious
+        }
+
+        public enum Acidity
+        {
+            Low,
+            MediumMinus,
+            Medium,
+            MediumPlus,
+            High
+        }
+
+        public enum Tannin
+        {
+            Low,
+            MediumMinus,
+            Medium,
+            MediumPlus,
+            High
+        }
+
+        public enum Nature
+        {
+            Ripe,
+            Soft,
+            Smooth,
+            Unripe,
+            Green,
+            Coarse,
+            Stalky,
+            Chalky,
+            FineGrained,
+        }
+    }
+}
