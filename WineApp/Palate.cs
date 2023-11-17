7d9e2c4ef96fcadff7f4081fc68bd7f49bf1fa4c
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:05:00 +0100

    Renmad Flavor class to Palate
    Edited members to mimick those on WSET level 4 tasting chart

---
+++ WineApp/Palate.cs
@@ -0,0 +1,15 @@
+﻿namespace WineApp
+{
+    public class Palate
+    {
+        public DefaultValues.PalateDefaults.Sweetness Sweetness { get; set; }
+        public DefaultValues.PalateDefaults.Acidity Acidity { get; set; }
+        public DefaultValues.PalateDefaults.Tannin Tannin { get; set; }
+        public DefaultValues.PalateDefaults.Nature Nature { get; set; }
+        public PrimaryValues PrimaryFlavors { get; set;}
+        public DefaultValues.SecondaryDefaults.Microbial SecondaryFlavors { get; set; }
+        public TertiaryValues TertiaryFlavors { get; set; }
+        public FaultValues FaultFlavors { get; set; }
+    }
+}
+
