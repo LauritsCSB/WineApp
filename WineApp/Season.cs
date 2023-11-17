2465f905904287668f7a44c3e6031eaa8fe7933c
Laurits Bentsen <laurits2@hotmail.com>
2023-11-17 22:11:44 +0100

    Basically added Climate member of type Climate

--- WineApp/Season.cs
+++ WineApp/Season.cs
@@ -4,17 +4,11 @@
     {
         public DateOnly YearOfSeason { get; set; }
         public Region RegionName { get; set; }
-
-        public List<double> MonthlyTemperatureAverage { get; set; }
-        public List<double> MonthlyTemperatureHigh { get; set; }
-        public List<double> MonthlyTemperatureLow { get; set; }
-        public List<double> MonthlyRainfall { get; set; }
-
         public DateOnly HarvestDate { get; set; }
-
         public bool FrostDuringBudbreak { get; set; }
         public int SpringDaysOfFrost { get; set; }
         public int AutumnDaysOfFrost { get; set; }
+
+        public Climate GetClimate { get; set; }
     }
 }
-
